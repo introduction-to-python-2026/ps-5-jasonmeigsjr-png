d23ed8f3b53a170ec25e6cfa352fbfc97301fd61
jasonmeigsjr-png <jasonmeigsjr@gmail.com>
2025-11-29 17:19:44 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,6 +1,6 @@
 # Add the import statements for necessary sympy functions here
 from sympy import solve as sympy_solve
-from sympy.core import symbols
+from sympy import symbols
 from sympy import Eq
 
 
